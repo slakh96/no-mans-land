6df60f004c084775540ecdf95cac1b49fc479a5f
slakh96 <shahzil.lakhani@mail.utoronto.ca>
2021-10-07 13:41:40 -0400

    Add updated spaceship with generous self-destruct time and update is kinematic with comments

--- NoMansLand/Assets/Scripts/DisableIsKinematic.cs
+++ NoMansLand/Assets/Scripts/DisableIsKinematic.cs
@@ -6,20 +6,27 @@ public class DisableIsKinematic : MonoBehaviour
 {
 	public GameObject cube;
 	public float time;
+
+	// A multiplier to control how long initially it takes for the ship to completely crumble, in units of 53s.
+	// E.g. timeMultiplier == 1.0 => 1 * 53s until spaceship finishes crumbling.
+	// timeMultiplier == 5.0 => 5 * 53s = 265s = 4min25s until it finishes crumbling. 
+	private float timeMultiplier = 5.0f;
+
+	private float adjustedTime;
     // Start is called before the first frame update
     void Start()
     {
-        
+	    adjustedTime = timeMultiplier * time;
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (time <= 0f)
+		if (adjustedTime <= 0f)
         {
         	Rigidbody cubeRigidbody = GetComponent<Rigidbody>();
         	cubeRigidbody.isKinematic = false;
         }
-        time -= Time.deltaTime;
+		adjustedTime -= Time.deltaTime;
     }
 }
