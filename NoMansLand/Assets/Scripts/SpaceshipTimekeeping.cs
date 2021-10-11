a2588fae6151505f657ae4cbc7d9d61d39b2bfa1
slakh96 <shahzil.lakhani@mail.utoronto.ca>
2021-10-10 20:35:21 -0400

    Initial script work on adding ability to add time

---
+++ NoMansLand/Assets/Scripts/SpaceshipTimekeeping.cs
@@ -0,0 +1,11 @@
+﻿namespace DefaultNamespace
+{
+    public static class SpaceshipTimekeeping
+    {
+        public static float BonusTime = 0;
+        // static void Starting()
+        // {
+        //     float timer2 = 3;
+        // }
+    }
+}
