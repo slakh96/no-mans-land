f08f0f27a246b7b2ab19ef6a73c4221aaa570d98
Shahzil Lakhani <s.lakhani@hotmail.ca>
2021-10-13 16:47:02 +0000

    Increase spaceship timer on deposit (#28)
    * Add functionality to increase time limit when a material is returned to the ship
    
    * Increase gravity by 3x; increase jump power by 3x

--- NoMansLand/Assets/Scripts/HandleObject.cs
+++ NoMansLand/Assets/Scripts/HandleObject.cs
@@ -1,15 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using DefaultNamespace;
 
 public class HandleObject : MonoBehaviour
 {
     private GameObject collectibleHoldSlot;
+    // The spaceship object - null if not in the game
+    private GameObject spaceship;
+    // How close the player needs to be before he can deposit the material successfully
+    private float DISTANCE_LIMIT = 50;
+    // Time bonus earned when the player deposits a material
+    private float TIME_BONUS = 30;
+    // Time to wait before the material is destroyed
+    private float DESTROY_DELAY = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         collectibleHoldSlot = GameObject.Find("CollectibleHolder");
+        spaceship = GameObject.FindGameObjectWithTag("Spaceship");
     }
     
     void Update()
@@ -21,10 +32,17 @@ public class HandleObject : MonoBehaviour
             Rigidbody collectibleRB = child.GetComponent<Rigidbody>();
             collectibleRB.isKinematic = false;
             collectibleRB.AddForce(-child.transform.forward * 20, ForceMode.Impulse);
+            // Check if the player was close enough to the spaceship to deposit the material
+            if (spaceship != null && 
+                Vector3.Distance(child.gameObject.transform.position, spaceship.transform.position) <= DISTANCE_LIMIT)
+            {
+                SpaceshipTimekeeping.AddBonusTime(TIME_BONUS);
+                Destroy(child, DESTROY_DELAY);
+            }
         }
     }
 
-    void OnCollisionEnter(Collision other) 
+    void OnCollisionEnter(Collision other)
     {
         if (other.collider.tag == "Collectible")
         {
@@ -33,7 +51,6 @@ public class HandleObject : MonoBehaviour
             collectibleObjTransform.localPosition = Vector3.zero;
             Rigidbody collectibleRB = other.gameObject.GetComponent<Rigidbody>();
             collectibleRB.isKinematic = true;
-
-        } 
+        }
     }
 }
