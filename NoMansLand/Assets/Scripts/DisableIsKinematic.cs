a8742327951d58b12a7e8126ee21bf6037fd98d6
slakh96 <shahzil.lakhani@mail.utoronto.ca>
2021-10-17 12:25:03 -0400

    Code cleanup

--- NoMansLand/Assets/Scripts/DisableIsKinematic.cs
+++ NoMansLand/Assets/Scripts/DisableIsKinematic.cs
@@ -12,34 +12,28 @@ public class DisableIsKinematic : MonoBehaviour
     {
 	    elapsedTime = 0;
 	    GameObject spaceship = GameObject.Find("spaceShip 1 2 1");
-	    Debug.Log("Begin ======================================");
-	    handleGameObjects(spaceship);
-	    Debug.Log("End=========================================");
+	    setOriginalPartData(spaceship);
     }
-
-    void handleGameObjects(GameObject spaceshipPartObj)
+	
+    // A recursive function to set the original positions and rotations of the spacship parts. Called at the start of the game
+    void setOriginalPartData(GameObject spaceshipPartObj)
     {
 	    if (spaceshipPartObj.transform.childCount == 0)
-	    {
-		   // Debug.Log(spaceshipPart.name);
+	    { 
 		   SpaceshipPart s = SpaceshipTimekeeping.GetSpaceshipPart(spaceshipPartObj.name);
 		   if (s == null)
 		   {
 			   Debug.Log("ERROR spaceship part not found in dictionary: " + spaceshipPartObj.name);
 			   return;
 		   }
-
 		   s.SetOriginalRelativePosition(spaceshipPartObj.transform.localPosition);
 		   s.SetOriginalRotation(spaceshipPartObj.transform.eulerAngles);
 		   return;
 	    }
 	    for (int i = 0; i < spaceshipPartObj.transform.childCount; i++)
 	    {
-		    //Debug.Log(spaceshipPart.transform.GetChild(i).gameObject.name);
-		    //Debug.Log(i);
-		    handleGameObjects(spaceshipPartObj.transform.GetChild(i).gameObject);
+		    setOriginalPartData(spaceshipPartObj.transform.GetChild(i).gameObject);
 	    }
-	    //Debug.Log(i);
     }
 
     // Update is called once per frame
@@ -51,22 +45,12 @@ public class DisableIsKinematic : MonoBehaviour
         	cubeRigidbody.isKinematic = false;
         }
 		elapsedTime += Time.deltaTime;
-		if (elapsedTime < 2)
-		{
-			GameObject first_piece = GameObject.Find("engine_frt_geo");
-			// Debug.Log(first_piece.transform.localPosition.x);
-			// Debug.Log(first_piece.transform.localPosition.y);
-			// Debug.Log(first_piece.transform.localPosition.z);
-			// Debug.Log("===================================");
-		}
+		
+		//TODO remove this and only call it when the player brings back a piece
 		if (elapsedTime > 7)
 		{
-			GameObject first_piece = GameObject.Find("engine_frt_geo");
-			SpaceshipTimekeeping.GetSpaceshipPart("engine_frt_geo").ReturnPieceToShip(first_piece);
-			// first_piece.transform.localPosition = new Vector3(-0.05929808f, 0.02065961f, -0.0006725601f);
-			// first_piece.transform.eulerAngles = new Vector3(0, 0, 0);
-			// first_piece.GetComponent<Rigidbody>().isKinematic = true;
-			// Debug.Log("===================================");
+			//GameObject first_piece = GameObject.Find("engine_frt_geo");
+			//SpaceshipTimekeeping.GetSpaceshipPart("engine_frt_geo").ReturnPieceToShip(first_piece);
 		}
     }
 }
