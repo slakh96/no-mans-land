cff23f2d4b3ff7741c8769f4e4e612ab475213c7
AbhiKapoor55 <abhishek.kapoor@mail.utoronto.ca>
2021-10-21 16:04:29 -0400

    modified random spawner code

--- NoMansLand/Assets/Scripts/RandomSpawner.cs
+++ NoMansLand/Assets/Scripts/RandomSpawner.cs
@@ -7,16 +7,16 @@ using DefaultNamespace;
 public class RandomSpawner : MonoBehaviour
 {
     public GameObject CollectiblePrefab;
-    public GameObject terrain;
+    public Terrain terrain;
     private List<GameObject> spaceShipParts = new List<GameObject>(); // A list of all the individual parts of the ship
     public int terrainXPos; // corner X position of the terrain 
     public int terrainYPos; // corner Y position of the terrain 
     public int terrainZPos; // corner Z position of the terrain 
-    public int terrainXLength = 1800; // length of terrain in X axis 
-    public int terrainZLength = 2000; // length of terrain in Z axis 
+    public int terrainXLength; // length of terrain in X axis 
+    public int terrainZLength; // length of terrain in Z axis 
     public int itemXPos; // x position of item 
     public int itemZPos; // z position of item 
-    public const int cushionAmount = 100; // to ensure spawning does not happen at the edges (at the borders)
+    public const int cushionAmount = 50; // to ensure spawning does not happen at the edges (at the borders)
     
     // Start is called before the first frame update
     void Start()
@@ -24,6 +24,12 @@ public class RandomSpawner : MonoBehaviour
         terrainXPos = (int)terrain.transform.position[0];
         terrainYPos = (int)terrain.transform.position[1];
         terrainZPos = (int)terrain.transform.position[2];
+
+        terrainXLength = (int)terrain.terrainData.size[0];
+        terrainZLength = (int)terrain.terrainData.size[2];
+
+        Debug.Log(terrain.terrainData.size);
+        Debug.Log(terrain.transform.position);
         StartCoroutine(CollectibleDrop());
     }
 
@@ -35,12 +41,12 @@ public class RandomSpawner : MonoBehaviour
         for (int i = 0; i < spaceShipParts.Count; i++)
         {
             // generate random X position in the range (terrainXPos + 100, terrainXPos + 900)
-            itemXPos = UnityEngine.Random.Range(terrainXPos - cushionAmount, terrainXPos + terrainXLength); 
-            // generate random Y position in the range (terrainZPos + 100, terrainZPos + 988)
-            itemZPos = UnityEngine.Random.Range(terrainZPos + 1100, terrainZPos + 1100 + terrainZLength);
+            itemXPos = UnityEngine.Random.Range(terrainXPos + cushionAmount, terrainXPos + terrainXLength - cushionAmount); 
+            // generate random Z position in the range (terrainZPos + 100, terrainZPos + 988)
+            itemZPos = UnityEngine.Random.Range(terrainZPos + cushionAmount, terrainZPos + terrainZLength - cushionAmount);
 
 			// Duplicate each spaceship part and spawn to a random location as a collectible
-            GameObject instantiatedClone = Instantiate(spaceShipParts[i], new Vector3(itemXPos, terrainYPos, itemZPos), Quaternion.identity);
+            GameObject instantiatedClone = Instantiate(spaceShipParts[i], new Vector3(itemXPos, terrainYPos + 5, itemZPos), Quaternion.identity);
 			// Size it using the same scale as the actual ship
 			instantiatedClone.transform.localScale = new Vector3(SpaceshipManager.SPACESHIP_SCALE, SpaceshipManager.SPACESHIP_SCALE, SpaceshipManager.SPACESHIP_SCALE);
 			instantiatedClone.tag = "Collectible";
