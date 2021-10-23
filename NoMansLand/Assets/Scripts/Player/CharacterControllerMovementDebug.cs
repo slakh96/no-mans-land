9020d259d353206bd6b560c839b9f29746e49c27
slakh96 <shahzil.lakhani@mail.utoronto.ca>
2021-10-22 20:37:55 -0400

    Move changes to another file

--- NoMansLand/Assets/Scripts/Player/CharacterControllerMovementDebug.cs
+++ NoMansLand/Assets/Scripts/Player/CharacterControllerMovementDebug.cs
@@ -1,8 +1,8 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DefaultNamespace;
-public class CharacterControllerMovement : MonoBehaviour
+public class CharacterControllerMovementDebug : MonoBehaviour
 {
     private CharacterController controller;
     public Transform cam;
@@ -13,7 +13,7 @@ public class CharacterControllerMovement : MonoBehaviour
 
     // Move
     private Vector3 move;
-    private float speed = 50f;
+    private float speed = GameSettings.IS_DEBUG_MODE ? 150f : 50f;
     private float turnSmoothTime = 0.1f;
     private float turnSmoothVelocity;
     
@@ -56,7 +56,7 @@ public class CharacterControllerMovement : MonoBehaviour
     
     void Jump()
     {
-        if (controller.isGrounded)
+        if (controller.isGrounded || GameSettings.IS_DEBUG_MODE)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravity);
         }
